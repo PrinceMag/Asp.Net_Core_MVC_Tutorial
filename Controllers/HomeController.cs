4878a93bae778c698a25b0e0ff37957f0cc84498
Prince Magutshwa <princeth.magutshwa@gmail.com>
2024-11-18 13:03:27 +0200

    Creating controllers and action methods

---
+++ Controllers/HomeController.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Asp.Net_Core_MVC_Tutorial.Controllers
+{
+    public class HomeController : Controller
+    {
+        //View name and action method is same
+
+        public ViewResult About()
+        {
+            return View();
+        }
+
+        //View name and action method is different
+        public ViewResult Contact()
+        {
+            return View("ContactUs");
+        }
+
+        //View resides in shared folder
+        public ViewResult Product()
+        {
+            return View();
+        }
+    }
+}
