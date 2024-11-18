20fdde8303b77d909863cc8876f3d47794118a18
Prince Magutshwa <princeth.magutshwa@gmail.com>
2024-11-18 13:14:47 +0200

    Learning on ViewResults for ActionResults

---
+++ Controllers/AccountController.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Asp.Net_Core_MVC_Tutorial.Controllers
+{
+    public class AccountController : Controller
+    {
+        public ViewResult Dashboard()
+        {
+            return View();
+        }
+
+        public ViewResult Register()
+        {
+            return View();
+        }
+
+        public ViewResult Login()
+        {
+            return View();
+        }
+
+
+    }
+}
--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -22,5 +22,50 @@ namespace Asp.Net_Core_MVC_Tutorial.Controllers
         {
             return View();
         }
+
+        //displaying view of different controller
+        public ViewResult Display()
+        {
+            return View("~/Views/Account/Dashboard.cshtml");
+        }
+
+
+        //localhost/home/index
+        public ViewResult Index()
+        {
+            return View();
+        }
+
+        //RedirectResult to internal url
+        public RedirectResult ShowInternal()
+        {
+            return Redirect("/home/index");
+        }
+
+        //RedirectResult to external url
+        public RedirectResult ShowExternal()
+        {
+            return Redirect("http://www.google.com");
+        }
+
+        public RedirectToActionResult ShowContact()
+        {
+            //return RedirectToRoute(new { controller = "Home", action = "contact" });
+            return RedirectToAction("contact");
+        }
+
+        public RedirectToActionResult SignUp()
+        {
+            return RedirectToAction("Register", "Account");
+        }
+
+        private void TestAnonymous()
+        {
+            var s1 = new
+            {
+                rollno = 5,
+                name = "Prince"
+            };
+        }
     }
 }
