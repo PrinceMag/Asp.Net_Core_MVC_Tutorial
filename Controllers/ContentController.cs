54a2ddcae3b335c7bda0480dc1a0445e59bb5776
Prince Magutshwa <princeth.magutshwa@gmail.com>
2024-11-18 13:19:05 +0200

    Returning and displaying Content Results

---
+++ Controllers/ContentController.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Asp.Net_Core_MVC.Controllers
+{
+    public class ContentController : Controller
+    {
+        public ContentResult Greet()
+        {
+            return Content("Hello Everyone");
+        }
+
+        public string Say()
+        {
+            return "Good Bye";
+        }
+
+        public int Add()
+        {
+            int x = 6, y = 4;
+            int z = x + y;
+            return z;
+        }
+    }
+}
